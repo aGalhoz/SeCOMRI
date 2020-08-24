8f16d9ae0e10ac0a35cd8d6db2abec1afb0238d7
AnaGalhoz37 <63655559+AnaGalhoz37@users.noreply.github.com>
2020-08-24 17:45:12 +0200

    Update trajectories_temporal.R

--- R/trajectories_temporal.R
+++ R/trajectories_temporal.R
@@ -1,3 +1,5 @@
+# read hospital map
+m <- png::readPNG("./translatum.png")
 
 # 1: staff and patients (union)
 data_staff_COVID_timeline_plot_new <- data_staff_COVID_timeline_plot_new[!is.na(data_staff_COVID_timeline_plot_new$x),]
@@ -47,3 +49,70 @@ staff_animation <- animate(
 )
 
 anim_save("staff_trajectories_timeline_staff_patients.gif",staff_animation)
+
+# 2: staff 
+p <- ggplot(data_staff_timeCOVID_timeline_plot, aes(x, y,color = fraction.staff)) + 
+  annotation_custom(xmin=1, ymin=1, xmax=2, ymax=2,    
+                    rasterGrob(m,width = unit(1,"npc"), 
+                               height = unit(1,"npc")))+
+  geom_point(aes(size = abs(fraction.staff)),show.legend = T,alpha = 0.7) +
+  scale_size_continuous(range = c(7, 15),name = "|Delta|",breaks = c(0.001,0.002,0.004),
+                        labels = c("0,1%","0,2%","0,4%")) +
+  scale_colour_gradientn(colors = RColorBrewer::brewer.pal(9, "Blues")[4:9],
+                         breaks = c(0,
+                                    max(data_staff_timeCOVID_timeline_plot$fraction.staff)),
+                         labels=c("Minimum","Maximum"))  +
+  scale_fill_distiller(palette=4, direction=1) +
+  labs(colour = "Relative number of infected COVID19 staff") +
+  xlim(1,2) +
+  ylim(1,2) + 
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), legend.position = "bottom",legend.box="vertical",
+        legend.margin = margin(10, 0, 0, 0)) +
+  guides(colour = guide_colourbar(title.position="top", title.hjust = 0.5,barwidth = 20,order = 1),
+         size = guide_legend(title.position="top", title.hjust = 0.5,order = 2)) 
+
+staff_animation <- p + transition_manual(date) + labs(title = 'Date: {current_frame}')
+date.sequence <- unique(data_staff_timeCOVID_timeline_plot$date)
+staff_animation <- animate(
+  plot = staff_animation, 
+  nframes = length(date.sequence), 
+  fps = 4, 
+  end_pause = 8
+)
+
+anim_save("staff_trajectories_timeline_COVIDpositive_v2.gif",staff_animation)
+
+# 3: patients
+p <- ggplot(timeline.sequence.data %>% filter(fraction.patient>0), aes(x, y,color = fraction.patient)) + 
+  annotation_custom(xmin=1, ymin=1, xmax=2, ymax=2,    
+                    rasterGrob(m,width = unit(1,"npc"), 
+                               height = unit(1,"npc")))+
+  geom_point(aes(size = abs(fraction.patient)),show.legend = T,alpha = 0.7) +
+  scale_size_continuous(range = c(10, 17),name = "|Delta|",breaks = c(0.0025,0.005,0.009),
+                        labels = c("0,25%","0,5%","0,9%")) +
+  scale_colour_gradientn(colors = RColorBrewer::brewer.pal(9, "Reds")[3:9],
+                         breaks = c(0,
+                                    max(data_staff_COVID_timeline_plot_new$fraction.patient)),
+                         labels=c("Minimum","Maximum"))  +
+  #geom_tile(alpha=0.55) +
+  scale_fill_distiller(palette=4, direction=1) +
+  labs(colour = "Relative number of COVID19 patient") +
+  xlim(1,2) +
+  ylim(1,2) + 
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+        panel.background = element_blank(), legend.position = "bottom",legend.box="vertical",
+        legend.margin = margin(10, 0, 0, 0)) +
+  guides(colour = guide_colourbar(title.position="top", title.hjust = 0.5,barwidth = 20,order = 1),
+         size = guide_legend(title.position="top", title.hjust = 0.5,order = 2)) 
+
+staff_animation <- p + transition_manual(date) + labs(title = 'Date: {current_frame}')
+date.sequence <- unique(data_staff_COVID_timeline_plot_new[data_staff_COVID_timeline_plot_new$freq.patient.per.area>0,]$date)
+staff_animation <- animate(
+  plot = staff_animation, 
+  nframes = length(date.sequence), 
+  fps = 4, 
+  end_pause = 8
+)
+
+anim_save("patient_trajectories_timeline_v2.gif",staff_animation)
